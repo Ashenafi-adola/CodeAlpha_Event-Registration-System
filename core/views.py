d3f08cc7dd31415f37c41fead515f427c9d03086
ashenafi <ashenafiadola05@gmail.com>
2026-04-18 13:26:07 +0300

    definded the user reginstration view

--- core/views.py
+++ core/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from . models import Event
 from .forms import EventForm
 from django.views import generic
 
+class Registration(generic.CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'core/authPage.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = 'signup'
+        return context
 
 class Home(generic.CreateView):
     model = Event
