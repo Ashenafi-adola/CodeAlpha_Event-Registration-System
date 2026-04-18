b4d4a8abc3f5100f68d2ec1d105a19ce176a8d65
ashenafi <ashenafiadola05@gmail.com>
2026-04-18 13:23:08 +0300

    definded the homepage view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from . models import Event
+from .forms import EventForm
+from django.views import generic
 
-# Create your views here.
+
+
+class Home(generic.CreateView):
+    model = Event
+    form_class = EventForm
+    template_name = 'core/index.html'
+    context_object_name = 'events'
+
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user)
